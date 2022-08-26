cd03201a4208711556e57027164207fa9e8283bc
pranjalgupt <guptapranjal54321@gmail.com>
2022-08-27 02:22:06 +0530

    day1

--- test2.java
+++ test2.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class test2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a<=2 ||a % 2 == 0){
+            System.out.println("NO");
+        }
+        else{
+        System.out.print("Yes"); 
+        }
+    }    
 }
